4a0c143de703808aa96ee2f504f4d107b996b62e
siiriylonen <117457514+siiriylonen@users.noreply.github.com>
2024-12-18 09:04:32 +0200

    [FINNA-2476] LIDO: Add video download link and video/audio file information (#3039)

--- module/Finna/src/Finna/RecordDriver/SolrLido.php
+++ module/Finna/src/Finna/RecordDriver/SolrLido.php
@@ -648,6 +648,9 @@ class SolrLido extends \VuFind\RecordDriver\SolrDefault implements \Laminas\Log\
                 if (in_array($type, $audioTypeKeys)) {
                     if ($audio = $this->getAudio($url, $format, $description)) {
                         $audioUrls = array_merge($audioUrls, $audio);
+                        if ($extraDetails = $this->getExtraDetails($resourceSet, $language)) {
+                            $audioUrls = array_merge($audioUrls, $extraDetails);
+                        }
                     }
                     continue;
                 }
@@ -655,6 +658,9 @@ class SolrLido extends \VuFind\RecordDriver\SolrDefault implements \Laminas\Log\
                 if (in_array($type, $videoTypeKeys)) {
                     if ($video = $this->getVideo($url, $format, $description)) {
                         $videoUrls = array_merge($videoUrls, $video);
+                        if ($extraDetails = $this->getExtraDetails($resourceSet, $language)) {
+                            $videoUrls = array_merge($videoUrls, $extraDetails);
+                        }
                     }
                     continue;
                 }
