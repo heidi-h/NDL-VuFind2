e53866d102227db738a4eab870307b3a499a6521
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2024-12-20 10:48:29 +0200

    [FINNA-2940] Display CollectionList tab for records which are part of a collection (#3116)

---
+++ module/Finna/src/Finna/RecordTab/CollectionList.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Collection list tab
+ *
+ * PHP version 8
+ *
+ * Copyright (C) The National Library of Finland 2024.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  RecordTabs
+ * @author   Juha Luoma <juha.luoma@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_tabs Wiki
+ */
+
+namespace Finna\RecordTab;
+
+/**
+ * Collection list tab
+ *
+ * @category VuFind
+ * @package  RecordTabs
+ * @author   Juha Luoma <juha.luoma@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_tabs Wiki
+ */
+class CollectionList extends \VuFind\RecordTab\CollectionList
+{
+    /**
+     * Is this tab active?
+     * Override to allow this tab to be displayed for records which are part of a collection.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        $driver = $this->getRecordDriver();
+        return $driver->tryMethod('isCollection') || $driver->tryMethod('getContainingCollections');
+    }
+}
